459274b43ebab208f2e97646a4656d9fe7d0837f
= <=>
2024-11-26 16:38:57 +0100

    upload project files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Practica Router Vue</h1>
+      <nav>
+        <!-- <RouterLink to="/">Home</RouterLink> -->
+        <!-- <RouterLink to="/about">About</RouterLink> -->
+      </nav>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+
+</style>
